c9345ab10f0d344f47ea09fb093567723c32fad9
Shingirayi Mandebvu <shingim1@gmail.com>
2020-06-18 22:40:08 +0200

    Added my each with index tests

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -7,7 +7,7 @@ describe Enumerable do
         it "Must return enum if no block given." do
             expect(arr.my_each).to be_a(Enumerator) 
         end
-        it "Must throw argument error if given Arguments ()." do 
+        it "Must throw argument error if given arguments ()." do 
             expect{arr.my_each(9)}.to raise_error(ArgumentError)
         end
         it "Must return original array if empty block given {}." do
@@ -15,6 +15,17 @@ describe Enumerable do
         end
         it "Must return the same value as each if block given." do
             expect(arr.my_each{|i| puts "Returns #{i}"}).to eql(arr.each{|i| puts "Returns #{i}"})
+        end
+    end
+    describe "#my_each_with_index" do
+        it "Must return enum if no block given." do
+            expect(arr.my_each_with_index).to be_a(Enumerator)
+        end
+        it "Must return original array if empty block given {}." do
+            expect(arr.my_each_with_index{}).to eql(arr.each_with_index{})
+        end
+        it "Must return same value as each_with_index if block given." do
+            expect(arr.my_each_with_index{|i, v| puts "Index #{i} => value #{v}"}).to eql(arr.each_with_index{|i, v| puts "Index #{i} => value #{v}"})
         end
     end
 end
